10ad24d157dbe4459e97540ecd38be7d8aba8b7d
aliho3en <alihossain.ahmadi@dci.education>
2022-11-10 13:47:11 +0100

    E api

--- src/Api/todoApi.js
+++ src/Api/todoApi.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     "https://react-course-a1940-default-rtdb.europe-west1.firebasedatabase.app",
-  timeout: 5000,
 });
 
 export default instance;
